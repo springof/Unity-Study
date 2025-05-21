d27490e84941240f3eff4b39ef82b93854fc7610
dople7 <lsy980414@naver.com>
2025-05-21 16:09:59 +0900

    Unity Study (05.21)
    ++ find 를 사용하여 object 넣기

--- Assets/02. Scripts/StudyComponent.cs
+++ Assets/02. Scripts/StudyComponent.cs
@@ -2,12 +2,14 @@ using UnityEngine;
 
 public class StudyComponent : MonoBehaviour
 {
-    public GameObject obj;
+    private GameObject obj;
 
     public string changeName; //유니티에서 설정할 문자열 변수
 
     void Start()
     {
+        obj = GameObject.Find("Main Camera");
+
         obj.name = changeName;
     }
 }
