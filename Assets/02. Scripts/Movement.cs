4afa9b463f7505f3ab47d1d4159612bd6fa39f87
dople7 <lsy980414@naver.com>
2025-05-22 17:44:07 +0900

    Unity Study (05.22)
    ++ GameObject
    ++ Create, Destroy

--- Assets/02. Scripts/Movement.cs
+++ Assets/02. Scripts/Movement.cs
@@ -6,23 +6,16 @@ public class Movement : MonoBehaviour
 
     void Update()
     {
-        //transform.position = transform.position + Vector3.forward * moveSpeed;
+        /// Input System
+        /// 이동 > WASD, 방향키
+        /// 점프 > Space
+        /// 발사 > 마우스 클릭
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
 
-        if (Input.GetKey(KeyCode.W)) //앞
-        {
-            transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.A)) //왼쪽
-        {
-            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.S)) //뒤
-        {
-            transform.position += Vector3.back * moveSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.D)) //오른쪽
-        {
-            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
-        }
+        Vector3 dir = new Vector3(h, 0, v);
+        Debug.Log($"현재 입력 : {dir}");
+
+        transform.position += dir * moveSpeed * Time.deltaTime;
     }
 }
--- Assets/02. Scripts/StudyComponent.cs
+++ Assets/02. Scripts/StudyComponent.cs
@@ -2,14 +2,42 @@ using UnityEngine;
 
 public class StudyComponent : MonoBehaviour
 {
-    private GameObject obj;
+    public GameObject obj;
+    public Mesh msh;
+    public Material mat;
 
-    public string changeName; //유니티에서 설정할 문자열 변수
-
+    public Transform objTf;
+    //public string changeName; //유니티에서 설정할 문자열 변수
     void Start()
     {
-        obj = GameObject.Find("Main Camera");
+        obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        //obj = GameObject.Find("Main Camera");
+        obj = GameObject.FindGameObjectWithTag("Player");
+        objTf = GameObject.FindGameObjectWithTag("Player").transform;
 
-        obj.name = changeName;
+        //obj.GetComponent<MeshRenderer>().enabled = false;
+        //obj.name = changeName;
+        Debug.Log($"<color=#FFFFFF>name: {obj.name}</color>");
+        Debug.Log($"tag: {obj.tag}");
+        Debug.Log($"layer: {obj.layer}");
+        Debug.Log($"position: {obj.transform.position}");
+        Debug.Log($"rotation: {obj.transform.rotation}");
+        Debug.Log($"scale: {obj.transform.localScale}");
+
+        Debug.Log($"Meshfilter: {obj.GetComponent<MeshFilter>().mesh}");
+        Debug.Log($"Material: {obj.GetComponent<MeshRenderer>().material}");
+    }
+
+    public void CreateCube()
+    {
+        obj = new GameObject("Cube");
+
+        obj.AddComponent<MeshFilter>();
+        obj.AddComponent<MeshFilter>().mesh = msh;
+
+        obj.AddComponent<MeshRenderer>();
+        obj.GetComponent<MeshRenderer>().material = mat;
+
+        obj.AddComponent<BoxCollider>();
     }
 }
