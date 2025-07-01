9d463fd65af6da6fd6d93c6ab178ce059927c7bb
dople7 <lsy980414@naver.com>
2025-07-01 16:32:25 +0900

    Unity Study (07.01)
    ++ MonsterIdle
    ++ MonsterPatrol
    ++ Monster Trace
    ++ Monster Attack

---
+++ Assets/02. Scripts/Knight/MonsterCore.cs
@@ -0,0 +1,82 @@
+﻿using Mono.Cecil;
+using UnityEngine;
+
+public abstract class MonsterCore : MonoBehaviour
+{
+    public enum MonsterState { IDLE, PATROL, TRACE, ATTACK }
+    private MonsterState monsterState = MonsterState.IDLE;
+
+    protected Animator animator;
+    protected Rigidbody2D monsterRb;
+    protected Collider2D monsterColl;
+
+    public Transform target;
+
+    public float hp;
+    public float speed;
+    protected float moveDir;
+    protected bool isTrace;
+
+    protected float targetDist;
+
+    protected virtual void Init(float hp, float speed)
+    {
+        this.hp = hp;
+        this.speed = speed;
+
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        animator = GetComponent<Animator>();
+        monsterRb = GetComponent<Rigidbody2D>();
+        monsterColl = GetComponent<Collider2D>();
+    }
+
+    private void Update()
+    {
+        targetDist = Vector2.Distance(transform.position, target.position);
+
+        Vector3 monsterDir = Vector3.right * moveDir; // 몬스터가 바라보는 방향
+        Vector3 playerDir = (transform.position - target.position).normalized; // 플레이어로부터 몬스터의 방향
+
+        float dotValue = Vector3.Dot(monsterDir, playerDir); // 방향의 내적값
+
+        isTrace = dotValue < -0.7f && dotValue >= -1f;
+        // 몬스터가 플레이어를 바라보고 있는지 확인 음수가 나오면 플레이어를 바라보고 있는 것
+
+        switch (monsterState)
+        {
+            case MonsterState.IDLE:
+                Idle();
+                break;
+            case MonsterState.PATROL:
+                Patrol();
+                break;
+            case MonsterState.TRACE:
+                Trace();
+                break;
+            case MonsterState.ATTACK:
+                Attack();
+                break;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Return"))
+        {
+            moveDir *= -1;
+            transform.localScale = new Vector3(moveDir, 1, 1);
+        }
+    }
+
+    public abstract void Idle();
+    public abstract void Patrol();
+    public abstract void Trace();
+    public abstract void Attack();
+
+    public void ChangeState(MonsterState newState)
+    {
+        if (monsterState != newState)
+            monsterState = newState;
+    }
+}
--- Assets/02. Scripts/OOP/Game/Monster/Goblin.cs
+++ Assets/02. Scripts/OOP/Game/Monster/Goblin.cs
@@ -1,10 +1,106 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Threading;
+using UnityEngine;
 
-public class Goblin : Monster
+public class Goblin : MonsterCore
 {
-    public override void Init()
+    private float timer = 0f;
+    private float idleTime, patrolTime;
+    private float traceDist = 5f;
+    private float attackDist = 2f;
+    private bool isAttack;
+
+    private void Start()
     {
-        hp = 3f;
-        moveSpeed = 3f;
+        Init(10f, 2f);
+    }
+
+    protected override void Init(float hp, float speed)
+    {
+        base.Init(hp, speed);
+    }
+
+    public override void Idle()
+    {
+        timer += Time.deltaTime;
+        if (timer >= idleTime)
+        {
+            timer = 0f;
+            moveDir = Random.Range(0, 2) == 1 ? 1 : -1;
+            transform.localScale = new Vector3(moveDir, 1, 1);
+            patrolTime = Random.Range(2f, 5f);
+            animator.SetBool("isRun", true);
+
+            ChangeState(MonsterState.PATROL);
+        }
+
+        if (targetDist <= traceDist && isTrace)
+        {
+            timer = 0f;
+            ChangeState(MonsterState.TRACE);
+            animator.SetBool("isRun", true);
+        }
+    }
+
+    public override void Patrol()
+    {
+        transform.position += Vector3.right * moveDir * speed * Time.deltaTime;
+
+        timer += Time.deltaTime;
+        if (timer >= patrolTime)
+        {
+            timer = 0f;
+            idleTime = Random.Range(2f, 3f);
+            animator.SetBool("isRun", false);
+
+            ChangeState(MonsterState.IDLE);
+        }
+
+        if (targetDist <= traceDist && isTrace)
+        {
+            timer = 0f;
+            ChangeState(MonsterState.TRACE);
+            animator.SetBool("isRun", true);
+        }
+    }
+
+    public override void Trace()
+    {
+        animator.SetBool("isRun", true);
+        var targetDir = (target.position - transform.position).normalized;
+
+        transform.position += Vector3.right * targetDir.x * speed * Time.deltaTime;
+
+        var scaleX = targetDir.x > 0 ? 1 : -1;
+        transform.localScale = new Vector3(scaleX, 1, 1);
+
+        if (targetDist > traceDist)
+        {
+            animator.SetBool("isRun", false);
+
+            ChangeState(MonsterState.IDLE);
+        }
+
+        if (targetDist <= attackDist)
+        {
+            animator.SetBool("isRun", false);
+            ChangeState(MonsterState.ATTACK);
+        }
+    }
+
+    public override void Attack()
+    {
+        if (!isAttack)
+            StartCoroutine(AttackRoutine());
+    }
+
+    IEnumerator AttackRoutine()
+    {
+        isAttack = true;
+        animator.SetTrigger("Attack");
+        yield return new WaitForSeconds(1f);
+
+        isAttack = false;
+        ChangeState(MonsterState.IDLE);
     }
 }
